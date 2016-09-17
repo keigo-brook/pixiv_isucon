5f27f6c5ccc3e159fa3423c06fa5da87399370e7
Keigo Ogawa <goog@brookbach.com>
2016-09-17 19:28:06 +0900

    add log settings

--- ruby/unicorn_config.rb
+++ ruby/unicorn_config.rb
@@ -1,3 +1,6 @@
 worker_processes 1
 preload_app true
 listen "127.0.0.1:8080"
+path = '/home/isucon/private_isu/webapp/ruby/'
+stderr_path path + "/log/stderr.log"
+stdout_path path + "/log/stdout.log"
